c5e81154d9f761803f0140b8590f273131469683
Даниил Радченко <37844948+danila032040@users.noreply.github.com>
2020-11-22 18:36:26 +0300

    Added IProductFileWorker<T>
    Also added MainService where will be implemented main functions for the Task

---
+++ ServiceLibrary/Interfaces/IProductFileWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ServiceLibrary.Interfaces
+{
+
+    /// <summary>
+    /// Interface for reading/writing products to file
+    /// </summary>
+    /// <typeparam name="T">Product</typeparam>
+    public interface IProductFileWorker<T>
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Read and returns List of Product from file
+        /// </summary>
+        /// <param name="filePath">File path</param>
+        /// <returns>List of BakeryProduct</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="FileLoadException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        List<T> ReadFromFile(string filePath);
+
+        /// <summary>
+        /// Write to file List of Product
+        /// </summary>
+        /// <param name="filePath">File path</param>
+        /// <param name="products">List of BakeryProduct for writing to file</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        void WriteToFile(string filePath, List<T> products);
+
+        #endregion
+
+    }
+
+}
---
+++ UnitTest/FileWorkerTests.cs
@@ -0,0 +1,21 @@
+﻿using DataLibrary.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLibrary.Interfaces;
+using ServiceLibrary.Services;
+using System;
+using System.IO;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class FileWorkerTests
+    {
+        private readonly string _filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "Resources", "products.txt");
+        private readonly IProductFileWorker<BakeryProduct> _fileWorker = new BakeryProductFileWorker();
+
+
+        
+
+
+    }
+}
